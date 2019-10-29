51bad9f93fafd2666e1ff6c0c6ee237f32e687ab
frank906 <ceccoangiolieri1@libero.it>
2019-10-30 00:15:27 +0100

    Update server.py

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ access_token_secret = 'iP5v6ZOFJ1WTzaK92ATt09hcJn2u6gVm7TCHplRIOWDXZ'
 
 #api = tweepy.API(auth)
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True,
 				   wait_on_rate_limit_notify=True)
